ee60bb658b6fc7e4fbcf4fb0039897ab159ca311
GNorfolk <georgenorfolk@hotmail.co.uk>
2017-10-01 10:25:07 +0100

    Added structure to controller file.

--- controllers/memes_controller.rb
+++ controllers/memes_controller.rb
@@ -8,11 +8,35 @@ class MemesController < Sinatra::Base
 	set :views, Proc.new { File.join(root, 'views')}
 
 	# Index
+	get '/memes' do
+		erb :"memes/index"
+	end
+
 	# New
+	get '/memes/new' do
+		erb :"memes/new"
+	end
+
 	# Create
+	post '/memes' do
+	end
+
 	# Show
+	get '/memes/:id' do
+		erb :"memes/show"
+	end
+
 	# Edit
+	get '/memes/:id/edit' do
+		erb :"memes/edit"
+	end
+
 	# Update
+	put '/memes/:id' do 
+	end
+
 	# Destroy
+	delete '/memes/:id' do
+	end
 
 end
